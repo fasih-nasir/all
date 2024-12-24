dc89a66aea2e53dbb140d050908f1c1fb0d0908c
Fasih-Nasir <154458171+fasih-nasir@users.noreply.github.com>
2024-12-25 04:41:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ setTimeout(function() {
 var all=[
   
     {name:"WIHR",cat:"react",href:"https://wihrconsultant.vercel.app/",img:"img/52.png"},
-    {name:"Vin Website",cat:"react",href:"https://www.vinauditspro.com/",img:"img/51.png"},
+    {name:"Vin Website",cat:"website",href:"https://www.vinauditspro.com/",img:"img/51.png"},
+    {name:"Clear-Vin",cat:"website",href:"https://clearvinpro.com/",img:"img/54.png"},
+    
     
     
     
